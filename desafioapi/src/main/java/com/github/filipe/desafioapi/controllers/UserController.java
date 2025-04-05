bf5581622ad7a653970144af8732604fc7c53db2
Jose Filipe <filipeandrade000@gmail.com>
2025-04-05 16:56:01 -0300

    adaptando os controllers

--- desafioapi/src/main/java/com/github/filipe/desafioapi/controllers/UserController.java
+++ desafioapi/src/main/java/com/github/filipe/desafioapi/controllers/UserController.java
@@ -1,84 +1,83 @@
 package com.github.filipe.desafioapi.controllers;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.github.filipe.desafioapi.entities.User;
+import com.github.filipe.desafioapi.controllers.dto.UserDto;
 import com.github.filipe.desafioapi.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+@CrossOrigin
 @RestController
-@RequestMapping(value = "/api/v1")
-@Tag(name = "User")
-public class UserController {
+@RequestMapping("/users")
+@Tag(name = "Users Controller", description = "RESTful API for managing users.")
+public record UserController(UserService userService) {
 
-	@Autowired
-	private UserService userService;
-
-	@GetMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
-	@Operation(summary = "Endpoint to list all users in a paginated from")
-	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Operation successful") })
-	public ResponseEntity<Page<User>> findAllPaged(@RequestParam(defaultValue = "0") Integer pageNumber,
-			@RequestParam(defaultValue = "10") Integer pageSize) {
-		Page<User> userPage = userService.findAllPaged(pageNumber, pageSize);
-		return new ResponseEntity<>(userPage, HttpStatus.OK);
+	@GetMapping
+	@Operation(summary = "Get all users", description = "Retrieve a list of all registered users")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Operation successful")
+	})
+	public ResponseEntity<List<UserDto>> findAll() {
+		var users = userService.findAll();
+		var usersDto = users.stream().map(UserDto::new).collect(Collectors.toList());
+		return ResponseEntity.ok(usersDto);
 	}
 
-	@GetMapping(value = "/users/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	@Operation(summary = "Endpoint to search user by id")
-	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Operation successful") })
-	public ResponseEntity<User> findById(@PathVariable Long id) {
-		User user = userService.findById(id);
-		return new ResponseEntity<User>(user, HttpStatus.OK);
+	@GetMapping("/{id}")
+	@Operation(summary = "Get a user by ID", description = "Retrieve a specific user based on its ID")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Operation successful"),
+			@ApiResponse(responseCode = "404", description = "User not found")
+	})
+	public ResponseEntity<UserDto> findById(@PathVariable Long id) {
+		var user = userService.findById(id);
+		return ResponseEntity.ok(new UserDto(user));
 	}
 
-	@PostMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
-	@Operation(summary = "Endpoint to create a user")
-	@ApiResponses(value = { @ApiResponse(responseCode = "201", description = "User created successfully"),
-			@ApiResponse(responseCode = "422", description = "Invalid user data provided") })
-
-	public ResponseEntity<User> save(@RequestBody User obj) {
-		User createdUser = userService.create(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdUser.getId())
+	@PostMapping
+	@Operation(summary = "Create a new user", description = "Create a new user and return the created user's data")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "User created successfully"),
+			@ApiResponse(responseCode = "422", description = "Invalid user data provided")
+	})
+	public ResponseEntity<UserDto> create(@RequestBody UserDto userDto) {
+		var user = userService.create(userDto.toModel());
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(user.getId())
 				.toUri();
-		return ResponseEntity.created(uri).body(createdUser);
+		return ResponseEntity.created(location).body(new UserDto(user));
 	}
 
-	@DeleteMapping("/users/{id}")
-	@Operation(summary = "Endpoint to remove a user", description = "Delete an existing user based on its ID")
-	@ApiResponses(value = { @ApiResponse(responseCode = "204", description = "User deleted successfully"),
-			@ApiResponse(responseCode = "404", description = "User not found") })
-	public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+	@PutMapping("/{id}")
+	@Operation(summary = "Update a user", description = "Update the data of an existing user based on its ID")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "User updated successfully"),
+			@ApiResponse(responseCode = "404", description = "User not found"),
+			@ApiResponse(responseCode = "422", description = "Invalid user data provided")
+	})
+	public ResponseEntity<UserDto> update(@PathVariable Long id, @RequestBody UserDto userDto) {
+		var user = userService.update(id, userDto.toModel());
+		return ResponseEntity.ok(new UserDto(user));
+	}
+
+	@DeleteMapping("/{id}")
+	@Operation(summary = "Delete a user", description = "Delete an existing user based on its ID")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "204", description = "User deleted successfully"),
+			@ApiResponse(responseCode = "404", description = "User not found")
+	})
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		userService.delete(id);
 		return ResponseEntity.noContent().build();
-	}
-
-	@PutMapping("/users/{id}")
-	@Operation(summary = "Endpoint to update a user", description = "Update the data of an existing user based on its ID")
-	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "User updated successfully"),
-			@ApiResponse(responseCode = "404", description = "User not found"),
-			@ApiResponse(responseCode = "422", description = "Invalid user data provided") })
-	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
-		User updatedUser = userService.update(id, user);
-		return new ResponseEntity<>(updatedUser, HttpStatus.OK);
 	}
 }
