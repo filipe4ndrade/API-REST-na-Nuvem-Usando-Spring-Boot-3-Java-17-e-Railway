f4b876708f4cc2171564a5258da74af196bc9ccc
Jose Filipe <filipeandrade000@gmail.com>
2025-04-06 15:47:32 -0300

    ajustando exceptions e global exceptions

--- desafioapi/src/main/java/com/github/filipe/desafioapi/services/exceptions/NotFoundException.java
+++ desafioapi/src/main/java/com/github/filipe/desafioapi/services/exceptions/NotFoundException.java
@@ -1,11 +1,7 @@
 package com.github.filipe.desafioapi.services.exceptions;
 
 public class NotFoundException extends BusinessException {
-
-    private static final long serialVersionUID = 1L;
-
-    public NotFoundException() {
-        super("Resource not found.");
+    public NotFoundException(String message) {
+        super(message);
     }
-
 }
